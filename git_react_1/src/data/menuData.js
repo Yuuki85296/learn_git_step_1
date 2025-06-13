c6a171b42e204702f9ddaf89eda662dbb115a469
mumei1231 <mmark2677@gmail.com>
2025-06-13 11:30:56 +0700

    menuItem

---
+++ git_react_1/src/data/menuData.js
@@ -0,0 +1,15 @@
+export const menuItems = [{
+    id:1,
+    name:"ข้าวหมูทอด",
+    price:50,
+    Image:"data:https://www.lemon8-app.com/seo/image?item_id=7388098063337832961&index=0&sign=22e3cd4d15b883c0fb7f4be2edd0ce1f",
+    desc:"ข้าวหมูทอด บ้านมาร์ค แสนหอมฉุย"
+},
+
+{
+    id:2,
+    name:"ข้าวผัดกุ้ง",
+    price:55,
+    Image:"data:https://www.lemon8-app.com/seo/image?item_id=7388098063337832961&index=0&sign=22e3cd4d15b883c0fb7f4be2edd0ce1f",
+    desc:"ข้าวผัดกุ้ง บ้านมาร์ค แสนหอมฉุย"
+}]
